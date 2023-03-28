ef5a9ba5d970ca5ad5ed4dac17a470051dc55fd9
Jordy Tak <jordy.tak@gmail.com>
2023-03-28 19:28:56 +0200

    another commit

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -28,9 +28,28 @@ class SendMailController extends Controller
 
     public function store(Request $request)
     {
+        $data = [];
+        $requestdata = $request->except('_token');
 
+        foreach($requestdata as $x => $val){
+        echo($x);
+        
+        if(strtolower($x) ==="email"||strtolower($x)==="e-mail"||strtolower($x)=== "e_mail"){
+            echo("called");
+            $data[$x]= 'required|email';
+        }
 
-        $request->validate(['email'=> 'required|email']);      
+        else{
+        $data[$x] = 'required';}
+        
+        }
+
+        //dd($data);
+        //$filterdata = $data->except('_token');
+
+        $request->validate($data);
+        dd($request);
+        //$request->validate(['email'=> 'required|email']);      
         Mail::to($request->input('email'))
         ->send(new Contact());
 
